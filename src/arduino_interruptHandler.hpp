76b3c7fa0756d65ef74e26a154a54a5ee86da25a
Alexander Tonn <alexandertonn@gmx.de>
2025-09-23 10:56:42 +0200

    repository structure modified

---
+++ src/arduino_interruptHandler.hpp
@@ -0,0 +1,11 @@
+#ifndef ARDUINO_INTERRUPT_HANDLER_HPP
+#define ARDUINO_INTERRUPT_HANDLER_HPP
+
+class ArduinoInterruptHandler {
+public:
+
+private:
+
+};
+
+#endif // ARDUINO_INTERRUPT_HANDLER_HPP
